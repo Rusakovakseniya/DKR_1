f7e7348aae1da08c9ba33d330ae9f84589ed5549
Rusakovakseniya <144375444+Rusakovakseniya@users.noreply.github.com>
2023-10-24 20:42:45 +0300

    Add files via upload

---
+++ DKR.pas
@@ -0,0 +1,14 @@
+﻿program zad1;
+var x,y:real;
+begin
+  x:=-12;
+  while x<=6 do
+  begin
+    if x < -10 then y:= x*68
+    else if (x >= -10) and (x < -3) then y := Power(x,0.1*x)
+    else if (x >= -3) and (x < 4) then y := cos(x) - 86
+    else y:= log10(x)/cos(2*x);
+    writeln('y в точке ',x:2:2,' равно ',y:2:2);
+    x:=x+0.1;
+  end;
+end.
